0a369fdd6a56e407bd4022c5a25dd8335ff203e4
Juan Mateu <juanma.mateu@gmail.com>
2024-08-20 14:51:10 -0400

    fix(factories): fix ProductFactory class

--- tests/factories.py
+++ tests/factories.py
@@ -32,8 +32,8 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
-    name = factory.FuzzyChoice(choices=["Hat","Pants","Shirt","Apple","Banana","Pots","Towels","Ford","Chevy","Hammer","Wrench"])
+    name = FuzzyChoice(choices=["Hat","Pants","Shirt","Apple","Banana","Pots","Towels","Ford","Chevy","Hammer","Wrench"])
     description = factory.Faker("text", max_nb_chars=250)
-    price = factory.FuzzyDecimal(0.5, 2000.0, 2) 
-    available = factory.FuzzyChoice(choices=[True,False])
-    category = factory.FuzzyChoice(choices=[Category.UNKNOWN,Category.CLOTHS,Category.FOOD,Category.HOUSEWARES,Category.AUTOMOTIVE,Category.TOOLS])
+    price = FuzzyDecimal(0.5, 2000.0, 2) 
+    available = FuzzyChoice(choices=[True,False])
+    category = FuzzyChoice(choices=[Category.UNKNOWN,Category.CLOTHS,Category.FOOD,Category.HOUSEWARES,Category.AUTOMOTIVE,Category.TOOLS])
