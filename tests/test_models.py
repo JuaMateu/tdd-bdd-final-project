42cf1b591bf54b86f35201e9cde7f60506fa31b4
Juan Mateu <juanma.mateu@gmail.com>
2024-08-20 16:02:58 -0400

    feat(test): add find by name testcase

--- tests/test_models.py
+++ tests/test_models.py
@@ -173,3 +173,16 @@ class TestProductModel(unittest.TestCase):
         # Assert if the length of the products list is equal to 5, to verify that the five products created in the previous step have been successfully added to the database.
         self.assertEqual(len(products), 5)
     
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
+    
