ac2fd69118e4d72a0b768c71b91d07bba33943b0
Juan Mateu <juanma.mateu@gmail.com>
2024-08-20 15:34:15 -0400

    feat(tests): testcase Update Product

--- tests/test_models.py
+++ tests/test_models.py
@@ -120,3 +120,28 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(Decimal(found_product.price), product.price)
         self.assertEqual(found_product.available, product.available)
         self.assertEqual(found_product.category, product.category)
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        # Set the ID of the product object to None and then call the create() method on the product.
+        product.id = None
+        product.create()
+        # Log the product object again after it has been created to verify that the product was created with the desired properties.
+        # Assert that the ID of the product object is not None after calling the create() method.
+        self.assertIsNotNone(product.id)
+        # Update the product in the system with the new property values using the update() method.
+        product.description = "testing"
+        original_id = product.id
+        product.update()
+        # Assert that the id is same as the original id but description property of the product object has been updated correctly after calling the update() method.
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # Fetch all the product back from the system.
+        products = Product.all()
+        # Assert the length of the products list is equal to 1 to verify that after updating the product, there is only one product in the system.
+        self.assertEqual(len(products), 1)
+        # Assert that the fetched product has id same as the original id.
+        self.assertEqual(products[0].id, original_id)
+        # Assert that the fetched product has the updated description.
+        self.assertEqual(products[0].description, "testing")
